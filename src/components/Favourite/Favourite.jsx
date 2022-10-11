f2a03805875725f49a056e981ee7cf8f852d3d52
Phan Thang <89295316+phanthang97202@users.noreply.github.com>
2022-10-12 00:32:16 +0700

    Update Favourite.js

--- src/components/Favourite/Favourite.jsx
+++ src/components/Favourite/Favourite.jsx
@@ -79,7 +79,9 @@ const Favourite = () => {
                     <button>Continue Watch</button>
                   </Link>
                   <button
-                    onClick={dataCartContext.handlerDeleteItem}
+                    onClick={() => {
+                      dataCartContext.handlerDeleteItem(index)
+                    }}
                     style={{ backgroundColor: "#2e2828" }}
                   >
                     &#8212;
